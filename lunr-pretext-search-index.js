62c6b74c8404984e7fe88d519ef7dcb96ee34d90
Taran Funk <taranfunk@unomaha.edu>
2025-05-12 22:01:59 +0000

    Latest build deployed.

--- lunr-pretext-search-index.js
+++ lunr-pretext-search-index.js
@@ -10,13 +10,76 @@ var ptx_lunr_docs = [
   "body": "  "
 },
 {
-  "id": "sec-section-name",
+  "id": "DefFunc",
   "level": "1",
-  "url": "sec-section-name.html",
+  "url": "DefFunc.html",
   "type": "Section",
   "number": "1.1",
-  "title": "Section Title",
-  "body": " Section Title  Text of section.  "
+  "title": "Definition of a Function",
+  "body": " Definition of a Function  A function is a rule that assigns to each input to exactly one output. The set of all inputs is called the domain of the function, and the set of all outputs is called the range of the function.  A function can be represented in several ways, including a table of values, a graph, or an equation. The most common representation is an equation, which describes the relationship between the input and output variables.  For example, the function f(x) = 2x + 3 assigns to each input x a unique output f(x). The domain of this function is all real numbers, and the range is also all real numbers.   "
+},
+{
+  "id": "Linear",
+  "level": "1",
+  "url": "Linear.html",
+  "type": "Chapter",
+  "number": "2",
+  "title": "LO2: Linear Functions",
+  "body": " LO2: Linear Functions  Content of the new chapter.  "
+},
+{
+  "id": "LinearFunctions",
+  "level": "1",
+  "url": "LinearFunctions.html",
+  "type": "Chapter",
+  "number": "3",
+  "title": "LO3: Composition &amp; Transformation",
+  "body": " LO3: Composition & Transformation  Content of the new chapter.  "
+},
+{
+  "id": "Exp",
+  "level": "1",
+  "url": "Exp.html",
+  "type": "Chapter",
+  "number": "4",
+  "title": "LO4: Exponential Functions",
+  "body": " LO4: Exponential Functions  Content of the new chapter.  "
+},
+{
+  "id": "InvLogs",
+  "level": "1",
+  "url": "InvLogs.html",
+  "type": "Chapter",
+  "number": "5",
+  "title": "LO5: Inverses &amp; Logarithms",
+  "body": " LO5: Inverses & Logarithms  Content of the new chapter.  "
+},
+{
+  "id": "Quad",
+  "level": "1",
+  "url": "Quad.html",
+  "type": "Chapter",
+  "number": "6",
+  "title": "LO6: Quadratic Functions",
+  "body": " LO6: Quadratic Functions  Content of the new chapter.  "
+},
+{
+  "id": "HO",
+  "level": "1",
+  "url": "HO.html",
+  "type": "Chapter",
+  "number": "7",
+  "title": "LO7: Higher Order Polynomials",
+  "body": " LO7: Higher Order Polynomials  Content of the new chapter.  "
+},
+{
+  "id": "Rational",
+  "level": "1",
+  "url": "Rational.html",
+  "type": "Chapter",
+  "number": "8",
+  "title": "LO8: Rational Functions",
+  "body": " LO8: Rational Functions  Content of the new chapter.  "
 },
 {
   "id": "backmatter-2",
