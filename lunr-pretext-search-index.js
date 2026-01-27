5bbaae491cd8a0b6b20959eb2561462c5e66c1ad
thfunk <taranfunk@unomaha.edu>
2026-01-27 12:27:28 -0600

    Latest build deployed.

--- lunr-pretext-search-index.js
+++ lunr-pretext-search-index.js
@@ -10,13 +10,157 @@ var ptx_lunr_docs = [
   "body": "  "
 },
 {
-  "id": "DefFunc",
+  "id": "DefinitionFunction",
   "level": "1",
-  "url": "DefFunc.html",
+  "url": "DefinitionFunction.html",
   "type": "Section",
   "number": "1.1",
   "title": "Definition of a Function",
-  "body": " Definition of a Function  A function is a rule that assigns to each input to exactly one output. The set of all inputs is called the domain of the function, and the set of all outputs is called the range of the function.  A function can be represented in several ways, including a table of values, a graph, or an equation. The most common representation is an equation, which describes the relationship between the input and output variables.  For example, the function f(x) = 2x + 3 assigns to each input x a unique output f(x). The domain of this function is all real numbers, and the range is also all real numbers.   "
+  "body": " Definition of a Function  In this section, we will definine a function and learn how to identify the input and output variables of graphs, tables, diagrams, and equations.    A function is a relationship between two variables where every input corresponds to exactly one output .    Below, in and we have two examples of relationships which are functions . Notice that each input A, B, and C is mapped to exactly one output value. Two inputs can share the same output value, but each input must have a unique output value.  For example; in the first diagram, , each input is going to a different output value. In the second diagram, , inputs B and C are both going to the same output value of Pear, but each input is still going to a single output value.      Bubble Diagram  Bubble diagram of a function.   top=1.75  mid=2  top=1.75  mid=2   Input   Output      Apple  Pear  Grape  A  B  C         This is a Function   Bubble Diagram  Bubble diagram of a function.   top=1.75  mid=2  top=1.75  mid=2   Input   Output      Apple  Pear  Grape  A  B  C        In contrast, the diagrams below are both examples of relationships that are not functions . There is an input which has two different outputs in each diagram.  In the first diagram, , input B is going to both output values of Apple and Pear. In the second diagram, , input A is going to both output values of Apple and Grape.       Bubble Diagram  Bubble diagram of a function.   top=1.75  mid=2  top=1.75  mid=2   Input   Output       Apple  Pear  Grape  A  B  C         This is a Function   Bubble Diagram  Bubble diagram of a function.   top=1.75  mid=2  top=1.75  mid=2   Input   Output       Apple  Pear  Grape  A  B  C        Depending on the situation, we may refer to the relationship between a function and its inputs and outputs as a mapping , a correspondence , or a rule . We also have several ways to represent functions. We can use a table , a graph , a diagram , or an equation .  So far we have seen functions represented as diagrams. Before we can move on the others, we need to learn about function notation.    The representation of a function is called function notation . The letter is the name of the function, and the letter is the input variable, and the letter is the output variable. We say that is a function of .     Function Notation     y=f(x)      Output     Function Name     Input         Let's look at some examples showing function relationships using function notation.    In the first diagram, , we could use function notation to represent the same relationship:   Grape. \"The input A is mapped to the output Grape.\"     Apple. \"The input B is mapped to the output Apple.\"     Pear. \"The input C is mapped to the output Pear.\"    We would first need to name the function as it was not given a name in the diagram.      The number of visitors, , to the Omaha Zoo is given by the function , where is the number of years since the year 2000.  In this example, the input variable is , which represents the number of years since 2000. The output variable is , which represents the number of visitors to the zoo. The name of the function is .      "
+},
+{
+  "id": "function-definition",
+  "level": "2",
+  "url": "DefinitionFunction.html#function-definition",
+  "type": "Definition",
+  "number": "1.1.1",
+  "title": "",
+  "body": "  A function is a relationship between two variables where every input corresponds to exactly one output .   "
+},
+{
+  "id": "Func-Bubble-Diagram-figure1",
+  "level": "2",
+  "url": "DefinitionFunction.html#Func-Bubble-Diagram-figure1",
+  "type": "Figure",
+  "number": "1.1.2",
+  "title": "",
+  "body": "   Bubble Diagram  Bubble diagram of a function.   top=1.75  mid=2  top=1.75  mid=2   Input   Output      Apple  Pear  Grape  A  B  C      "
+},
+{
+  "id": "Func-Bubble-Diagram-figure2",
+  "level": "2",
+  "url": "DefinitionFunction.html#Func-Bubble-Diagram-figure2",
+  "type": "Figure",
+  "number": "1.1.3",
+  "title": "",
+  "body": "  This is a Function   Bubble Diagram  Bubble diagram of a function.   top=1.75  mid=2  top=1.75  mid=2   Input   Output      Apple  Pear  Grape  A  B  C      "
+},
+{
+  "id": "not-Func-Bubble-Diagram-figure1",
+  "level": "2",
+  "url": "DefinitionFunction.html#not-Func-Bubble-Diagram-figure1",
+  "type": "Figure",
+  "number": "1.1.4",
+  "title": "",
+  "body": "    Bubble Diagram  Bubble diagram of a function.   top=1.75  mid=2  top=1.75  mid=2   Input   Output       Apple  Pear  Grape  A  B  C      "
+},
+{
+  "id": "not-Func-Bubble-Diagram-figure2",
+  "level": "2",
+  "url": "DefinitionFunction.html#not-Func-Bubble-Diagram-figure2",
+  "type": "Figure",
+  "number": "1.1.5",
+  "title": "",
+  "body": "  This is a Function   Bubble Diagram  Bubble diagram of a function.   top=1.75  mid=2  top=1.75  mid=2   Input   Output       Apple  Pear  Grape  A  B  C      "
+},
+{
+  "id": "DefinitionFunction-10",
+  "level": "2",
+  "url": "DefinitionFunction.html#DefinitionFunction-10",
+  "type": "Paragraph (with a defined term)",
+  "number": "",
+  "title": "",
+  "body": "mapping correspondence rule table graph diagram equation "
+},
+{
+  "id": "function-notation-definition",
+  "level": "2",
+  "url": "DefinitionFunction.html#function-notation-definition",
+  "type": "Definition",
+  "number": "1.1.6",
+  "title": "",
+  "body": "  The representation of a function is called function notation . The letter is the name of the function, and the letter is the input variable, and the letter is the output variable. We say that is a function of .     Function Notation     y=f(x)      Output     Function Name     Input        "
+},
+{
+  "id": "function-notation-diagram-example",
+  "level": "2",
+  "url": "DefinitionFunction.html#function-notation-diagram-example",
+  "type": "Example",
+  "number": "1.1.8",
+  "title": "",
+  "body": "  In the first diagram, , we could use function notation to represent the same relationship:   Grape. \"The input A is mapped to the output Grape.\"     Apple. \"The input B is mapped to the output Apple.\"     Pear. \"The input C is mapped to the output Pear.\"    We would first need to name the function as it was not given a name in the diagram.   "
+},
+{
+  "id": "function-notation-wordproblem-example",
+  "level": "2",
+  "url": "DefinitionFunction.html#function-notation-wordproblem-example",
+  "type": "Example",
+  "number": "1.1.9",
+  "title": "",
+  "body": "  The number of visitors, , to the Omaha Zoo is given by the function , where is the number of years since the year 2000.  In this example, the input variable is , which represents the number of years since 2000. The output variable is , which represents the number of visitors to the zoo. The name of the function is .   "
+},
+{
+  "id": "EvaluatingFunctions",
+  "level": "1",
+  "url": "EvaluatingFunctions.html",
+  "type": "Section",
+  "number": "1.2",
+  "title": "Evaluating Functions",
+  "body": " Evaluating Functions  In this section, we will use function notation to evaluate functions. We will also learn how to interpret the meaning of the inputs and outputs of a function.    "
+},
+{
+  "id": "DomainRange",
+  "level": "1",
+  "url": "DomainRange.html",
+  "type": "Section",
+  "number": "1.3",
+  "title": "Domain and Range",
+  "body": " Domain and Range  In this section, we will learn how to identify, through a function's graph, the domain and range of a function. We will also learn how to determine where a function is increasing or decreasing.    "
+},
+{
+  "id": "IncreasingDecreasing",
+  "level": "1",
+  "url": "IncreasingDecreasing.html",
+  "type": "Section",
+  "number": "1.4",
+  "title": "Increasing and Decreasing",
+  "body": " Increasing and Decreasing  In this section, we will look at graphs to determine where a function is increasing or decreasing. We will also learn how to identify the intervals where a function is increasing or decreasing.    Read-Watch-Try        The graph of the function is shown below. On what interval or intervals is increasing?     f(x)=-(x-1)^2+2                                    The graph of the function is shown below. On what interval or intervals is decreasing?     f(x)=-(x-1)^2+2                                    The graph of the function is shown below. On what interval or intervals is increasing?     f(x)=1\/8.25*(x+3.5)*x*(x-3.5)  a = 1                                    The graph of the function is shown below. On what interval or intervals is decreasing?     f(x)=1\/8.25*(x+3.5)*x*(x-3.5)  a = 1                                  "
+},
+{
+  "id": "RWT-Increasing-Decreasing-exercise1",
+  "level": "2",
+  "url": "IncreasingDecreasing.html#RWT-Increasing-Decreasing-exercise1",
+  "type": "Exercise",
+  "number": "1.4.1",
+  "title": "",
+  "body": "  The graph of the function is shown below. On what interval or intervals is increasing?     f(x)=-(x-1)^2+2                                "
+},
+{
+  "id": "RWT-Increasing-Decreasing-exercise2",
+  "level": "2",
+  "url": "IncreasingDecreasing.html#RWT-Increasing-Decreasing-exercise2",
+  "type": "Exercise",
+  "number": "1.4.2",
+  "title": "",
+  "body": "  The graph of the function is shown below. On what interval or intervals is decreasing?     f(x)=-(x-1)^2+2                                "
+},
+{
+  "id": "RWT-Increasing-Decreasing-exercise3",
+  "level": "2",
+  "url": "IncreasingDecreasing.html#RWT-Increasing-Decreasing-exercise3",
+  "type": "Exercise",
+  "number": "1.4.3",
+  "title": "",
+  "body": "  The graph of the function is shown below. On what interval or intervals is increasing?     f(x)=1\/8.25*(x+3.5)*x*(x-3.5)  a = 1                                "
+},
+{
+  "id": "RWT-Increasing-Decreasing-exercise4",
+  "level": "2",
+  "url": "IncreasingDecreasing.html#RWT-Increasing-Decreasing-exercise4",
+  "type": "Exercise",
+  "number": "1.4.4",
+  "title": "",
+  "body": "  The graph of the function is shown below. On what interval or intervals is decreasing?     f(x)=1\/8.25*(x+3.5)*x*(x-3.5)  a = 1                                "
 },
 {
   "id": "Linear",
@@ -73,13 +217,58 @@ var ptx_lunr_docs = [
   "body": " LO7: Higher Order Polynomials  Content of the new chapter.  "
 },
 {
-  "id": "Rational",
+  "id": "sec-long-run-behavior-of-rationals",
   "level": "1",
-  "url": "Rational.html",
-  "type": "Chapter",
-  "number": "8",
-  "title": "LO8: Rational Functions",
-  "body": " LO8: Rational Functions  Content of the new chapter.  "
+  "url": "sec-long-run-behavior-of-rationals.html",
+  "type": "Section",
+  "number": "8.1",
+  "title": "Long-Run Behavior of Rational Functions",
+  "body": " Long-Run Behavior of Rational Functions  In this section, we will concern ourselves with the long-run behavior of rational functions.  Consider the graph of the function       f(x)=(2*x*(x+1))\/((x+2)*(x-1))            Just like with polynomial functions, we can imagine zooming way out on the graph, and we want a way to describe what the far left and far right ends of the graph are doing.      f(x)=(2*x*(x+1))\/((x+2)*(x-1))            This shows us that, when our -values become very large and negative, our -values become very close to , and when our -values become very large and positive, our -values become very close to as well.  Using our symbolic end behavior notation, we can write this as and   But why does this happen? Let's revisit our function's equation: If we multiply out the numerator and the denominator of the function, we can rewrite the function as   "
+},
+{
+  "id": "sec-long-run-behavior-of-rationals-4",
+  "level": "2",
+  "url": "sec-long-run-behavior-of-rationals.html#sec-long-run-behavior-of-rationals-4",
+  "type": "Figure",
+  "number": "8.1.1",
+  "title": "",
+  "body": "    f(x)=(2*x*(x+1))\/((x+2)*(x-1))           "
+},
+{
+  "id": "sec-long-run-behavior-of-rationals-6",
+  "level": "2",
+  "url": "sec-long-run-behavior-of-rationals.html#sec-long-run-behavior-of-rationals-6",
+  "type": "Figure",
+  "number": "8.1.2",
+  "title": "",
+  "body": "    f(x)=(2*x*(x+1))\/((x+2)*(x-1))           "
+},
+{
+  "id": "sec-short-run-behavior-of-rationals",
+  "level": "1",
+  "url": "sec-short-run-behavior-of-rationals.html",
+  "type": "Section",
+  "number": "8.2",
+  "title": "Short-Run Behavior of Rational Functions",
+  "body": " Short-Run Behavior of Rational Functions  Content of the new chapter.  "
+},
+{
+  "id": "sec-sketching-graphs-of-rationals",
+  "level": "1",
+  "url": "sec-sketching-graphs-of-rationals.html",
+  "type": "Section",
+  "number": "8.3",
+  "title": "Sketching Graphs of Rational Functions",
+  "body": " Sketching Graphs of Rational Functions  Content of the new chapter.  "
+},
+{
+  "id": "sec-writing-equations-of-rationals",
+  "level": "1",
+  "url": "sec-writing-equations-of-rationals.html",
+  "type": "Section",
+  "number": "8.4",
+  "title": "Writing Equations of Rational Functions",
+  "body": " Writing Equations of Rational Functions  Content of the new chapter.  "
 },
 {
   "id": "backmatter-2",
